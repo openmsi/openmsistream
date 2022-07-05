b520ac2d0ce6c9f1cd5b1cad8bdbd8969432ed71
Maggie Eminizer <margaret.eminizer@gmail.com>
2022-07-05 12:57:51 -0400

    document OpenMSIStreamKafkaCrypto

--- openmsistream/kafka_wrapper/openmsistream_kafka_crypto.py
+++ openmsistream/kafka_wrapper/openmsistream_kafka_crypto.py
@@ -5,30 +5,46 @@ from ..utilities.misc import cd
 
 class OpenMSIStreamKafkaCrypto :
     """
-    A class to own and work with the Producers, Consumers, and other objects needed
+    A wrapper class to own and work with the Producers, Consumers, and other objects needed
     by KafkaCrypto to handle encrypting/decrypting messages and exchanging keys
+
+    :param broker_configs: the producer/consumer-agnostic configurations for connecting to the broker
+    :type broker_configs: dict
+    :param config_file: the path to the KafkaCrypto config file for the node being used
+    :type config_file: :class:`pathlib.Path`
     """
 
     @property
     def key_serializer(self) :
+        """
+        The crypto key serializer
+        """
         return self._kc.getKeySerializer()
     @property
     def key_deserializer(self) :
+        """
+        The crypto key deserializer
+        """
         return self._kc.getKeyDeserializer()
     @property
     def value_serializer(self) :
+        """
+        The crypto value serializer
+        """
         return self._kc.getValueSerializer()
     @property
     def value_deserializer(self) :
+        """
+        The crypto value deserializer
+        """
         return self._kc.getValueDeserializer()
 
-    def __init__(self,server_configs,config_file) :
+    def __init__(self,broker_configs,config_file) :
         """
-        server_configs = the producer/consumer-agnostic configurations for connecting to the server in question
-        config_file    = the path to the config file that should be used to instantiate KafkaCrypto 
+        Constructor method
         """
-        producer_configs = server_configs.copy()
-        consumer_configs = server_configs.copy()
+        producer_configs = broker_configs.copy()
+        consumer_configs = broker_configs.copy()
         #figure out a consumer group ID to use (KafkaCrypto Consumers need one)
         if 'group.id' not in consumer_configs.keys() :
             consumer_configs['group.id'] = str(uuid.uuid1())
@@ -40,6 +56,9 @@ class OpenMSIStreamKafkaCrypto :
             self._kc = KafkaCrypto(None,self._kcp,self._kcc,config_file)
 
     def close(self) :
+        """
+        Close the :class:`kafkacrypto.KafkaCrypto` object and un-set its producer/consumer
+        """
         try :
             self._kc.close()
         except :
