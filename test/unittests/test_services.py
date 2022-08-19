80d0e2db0d9b9c4b5aaa4cf2c0e3679685e8c55c
Maggie Eminizer <margaret.eminizer@gmail.com>
2022-08-19 11:26:03 -0400

    getting there

--- test/unittests/test_services.py
+++ test/unittests/test_services.py
@@ -62,7 +62,7 @@ class TestServices(unittest.TestCase) :
                                                 logger=LOGGER)
                 manager.install_service()
                 for run_mode in ('start','status','stop','remove','reinstall','remove') :
-                    time.sleep(5 if run_mode=='status' else 1)
+                    time.sleep(1)
                     manager.run_manage_command(run_mode,False,False)
                 time.sleep(1)
                 error_log_path = pathlib.Path().resolve()/f'{service_name}{SERVICE_CONST.ERROR_LOG_STEM}'
@@ -100,7 +100,7 @@ class TestServices(unittest.TestCase) :
                                                 logger=LOGGER)
                 manager.install_service()
                 for run_mode in ('start','status','stop','remove','reinstall','remove') :
-                    time.sleep(5 if run_mode=='status' else 1)
+                    time.sleep(1)
                     manager.run_manage_command(run_mode,False,False)
                 time.sleep(1)
                 self.assertFalse((SERVICE_CONST.DAEMON_SERVICE_DIR/f'{service_name}.service').exists())
