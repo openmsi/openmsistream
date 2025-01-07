f00462673bb3960ba166c61c366ae7d33c9e9bdb
Tyrel M. McQueen <tmcqueen-materials@github.com>
2025-01-06 19:44:51 -0500

    Require compatible openmsitoolbox version.

--- setup.py
+++ setup.py
@@ -59,7 +59,7 @@ setupkwargs = dict(
         "matplotlib",
         "methodtools",
         "msgpack",
-        "openmsitoolbox>=1.2.3",
+        "openmsitoolbox>=1.2.4",
         "watchdog>=3.0.0",
     ],
     extras_require={
