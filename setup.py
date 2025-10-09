a70c653b0dd0505d99959284d21d6fc9b4102180
arachid1 <arachid1@jhu.edu>
2025-10-09 12:20:13 -0400

    adding poetry

--- setup.py
+++ setup.py
@@ -52,7 +52,8 @@ setupkwargs = dict(
         "matplotlib",
         "methodtools",
         "msgpack",
-        "openmsitoolbox==1.2.5.dev1",
+        # "openmsitoolbox==1.2.5.dev1",
+        "openmsitoolbox @ file:///home/arachid1/openmsitoolbox",
         "watchdog>=3.0.0",
     ],
     extras_require={
