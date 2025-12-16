dc14f9d66e1633a25655c5fd493ba593f7dd45f3
Ali Rachidi <ali.rachidi73@gmail.com>
2025-12-16 15:09:41 +0100

    deleting old_test placeholder folder

--- old_test/start_local_broker.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-set -euxo pipefail
-
-docker compose -f local-kafka-broker-docker-compose.yml up -d
--- old_test/stop_local_broker.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-set -euxo pipefail
-
-docker compose -f local-kafka-broker-docker-compose.yml down -t 0
